d4f59a15d45b9fbcda7013b787c4baaa86bc982a
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-09-26 18:46:31 -0400

    -added User repository

---
+++ src/main/java/com/growthhungry/hungrio/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.growthhungry.hungrio.repository;
+
+import com.growthhungry.hungrio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+}
