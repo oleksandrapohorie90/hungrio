15c1cfa858dc28f876c40f6689bdd0e06108c09a
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-09-28 16:40:10 -0400

    -added task 2 and task 3 Create a UserService interface and a UserServiceImpl class within the service package. This service will encapsulate the core business logic for user management. Develop an AuthController within the controller package to expose public endpoints for authentication. This controller should be annotated with @RestController and include mapped methods

--- src/main/java/com/growthhungry/hungrio/config/SecurityConfig.java
+++ src/main/java/com/growthhungry/hungrio/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.growthhungry.hungrio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -14,7 +16,19 @@ public class SecurityConfig {
         //default strength (log rounds) = 10; you can raise to 12 for stronger hashing
         //BCryptPasswordEncoder uses BCrypt with a per-password salt and a tunable cost factor (default 10). Higher strength = slower hashing = better brute-force resistance.
         return new BCryptPasswordEncoder();
-       //This exposes a singleton BCrypt encoder in the Spring context for dependency injection anywhere (e.g., your service). BCrypt is the recommended one-way hashing algorithm in Spring Security.
+        //This exposes a singleton BCrypt encoder in the Spring context for dependency injection anywhere (e.g., your service). BCrypt is the recommended one-way hashing algorithm in Spring Security.
     }
+
+    @Bean
+    SecurityFilterChain security(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/register", "/api/auth/login", "/hello").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
 }
 
